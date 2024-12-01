12255c514f9264a286fdf66b4246f7f28a7527f7
Mendrika Rakotomanga <mgrakotomanga@gmail.com>
2024-12-01 10:20:51 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,2 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-
-a = 1
-x = np.arange(1, 100)
-
-
-st.title("My first streamlit app")
-
-st.write(a)
-
 st.write("Hello World")
